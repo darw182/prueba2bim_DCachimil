728095c53ee71a18ba3414bf8a03497f17f1a38c
Darwin <darwin.cachimil@epn.edu.ec>
2024-07-12 11:15:36 -0500

    Primer commit

---
+++ src/login.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class login {
+    private JTextField userText;
+    private JTextField passText;
+    private JButton ingresarBtn;
+    private JButton resetBtn;
+    private JPanel mainPanel;
+
+    
+}
