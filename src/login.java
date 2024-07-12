79439634a57779fc1d4cd0b0ccbb4070a8d00ba4
Darwin <darwin.cachimil@epn.edu.ec>
2024-07-12 11:26:33 -0500

    segundo commit

--- src/login.java
+++ src/login.java
@@ -1,4 +1,10 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class login {
     private JTextField userText;
@@ -6,6 +12,40 @@ public class login {
     private JButton ingresarBtn;
     private JButton resetBtn;
     private JPanel mainPanel;
+    private Connection connection;
+    private PreparedStatement preparedStatement;
 
-    
+
+    public login(){
+        String url = "jdbc:mysql://localhost:3306/sistema_hospitalario";
+        String user = "root";
+        String password = "123456";
+
+        try {
+            connection= DriverManager.getConnection(url,user,password);
+            System.out.println("Exito");
+            //String queryInsert = "insert into info_usuario values(?,?,?,?,?,?)";
+            //preparedStatement = connection.prepareStatement(queryInsert);
+
+
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+        ingresarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("login");
+        frame.setContentPane(new login().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
